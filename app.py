005bc6848d050a5f1bd01942391c870c26e1f94b
Tsunderarislime <terrarislime@gmail.com>
2025-06-11 11:24:42 -0700

    Completed basic image upload logic

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, abort, send_from_directory
+from celery import Celery
+import os
+from time import time
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '1'
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 4
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+# Make the uploads folder, if it does not already exist
+os.makedirs(app.config['UPLOAD_PATH'], exist_ok=True)
+
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['result_backend'] = 'redis://localhost:6379/0'
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+# Set up uploads folder cleaning task, every 5 minutes
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender: Celery, **kwargs):
+    sender.add_periodic_task(300.0, clean.s(), name='Clean Uploads Every 5 Minutes', expires=60.0)
+
+# TODO: Process the image
+@celery.task
+def process_image(image_path, slider_value, numeric_value):
+    return slider_value + numeric_value
+
+# Cleanup task, remove images in the uploads folder if they've been there for over an hour.
+@celery.task
+def clean():
+    images = [os.path.join(app.config['UPLOAD_PATH'], im) for im in os.listdir('uploads')]
+    for im in images:
+        if time() - im > 3600:
+            os.remove(im)
+
+# Temporary, display each image in the uploads folder
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+# Get file from upload
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+
+    return redirect(url_for('index'))
+
+# Temporary, image source for uploads display
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+# Debug mode if run directly
+if __name__ == '__main__':
+    app.run(debug=True)
