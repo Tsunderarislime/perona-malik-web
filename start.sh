6e7ac562779f2de783cdd077dbe521af6c190479
Tsunderarislime <terrarislime@gmail.com>
2025-06-16 13:43:46 -0700

    Removed start shell script, since it's no longer used

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Start celery workers
-celery -A app.celery worker --loglevel=INFO &
-celery -A app.celery beat --loglevel=INFO &
-
-# Start gunicorn
-gunicorn --bind 0.0.0.0:8000 app:app
